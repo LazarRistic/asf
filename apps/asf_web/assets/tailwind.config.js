abdcafe050263c324838a3a014b70fb8a1219f0e
Lazar Ristic <lazar.ristic@vibe.rs>
2021-03-31 22:38:54 +0200

    Fix Settings page

--- apps/asf_web/assets/tailwind.config.js
+++ apps/asf_web/assets/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     '../lib/**/*.ex',
@@ -11,6 +13,17 @@ module.exports = {
       listStyleType: 'list-disc',
       listStylePosition: 'list-inside'
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      input_error: 'var(--color-input-error)'
+    }
   },
   variants: {
     extend: {},
