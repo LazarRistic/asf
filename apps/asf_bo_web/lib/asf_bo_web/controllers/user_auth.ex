49136b0a902169044e386168ba546ae08dfaae12
Lazar Ristic <lazar.ristic@vibe.rs>
2021-04-02 09:27:58 +0200

    [asf_bo_web] Add can not sign in if not admin

--- apps/asf_bo_web/lib/asf_bo_web/controllers/user_auth.ex
+++ apps/asf_bo_web/lib/asf_bo_web/controllers/user_auth.ex
@@ -128,14 +128,21 @@ defmodule AsfBOWeb.UserAuth do
   they use the application at all, here would be a good place.
   """
   def require_authenticated_user(conn, _opts) do
-    if conn.assigns[:current_user] do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must log in to access this page.")
-      |> maybe_store_return_to()
-      |> redirect(to: Routes.user_session_path(conn, :new))
-      |> halt()
+    case conn.assigns[:current_user] do
+      nil ->
+        conn
+        |> put_flash(:error, "You must log in to access this page.")
+        |> maybe_store_return_to()
+        |> redirect(to: Routes.user_session_path(conn, :new))
+        |> halt()
+
+      %{roles: ["admin"]} ->
+        conn
+
+      _ ->
+        conn
+        |> put_flash(:error, "You do not have the rights to access this page.")
+        |> log_out_user()
     end
   end
 
